f536d324a30802f6c8e6099931c74a8fdd10507a
Jefferson Ortiz <jortizr@ut.edu.co>
2025-04-01 17:52:37 -0500

    Creacion de interfaz y funciones de los botones portapapeles

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -53,7 +53,7 @@
             this.Ciudad_Destino});
             this.dgvGuias.Location = new System.Drawing.Point(23, 79);
             this.dgvGuias.Name = "dgvGuias";
-            this.dgvGuias.Size = new System.Drawing.Size(330, 337);
+            this.dgvGuias.Size = new System.Drawing.Size(330, 428);
             this.dgvGuias.TabIndex = 0;
             // 
             // label1
@@ -118,7 +118,7 @@
             this.Estado_Guia});
             this.dataGridView1.Location = new System.Drawing.Point(492, 78);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(660, 338);
+            this.dataGridView1.Size = new System.Drawing.Size(660, 429);
             this.dataGridView1.TabIndex = 5;
             // 
             // Guias
@@ -149,7 +149,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1164, 450);
+            this.ClientSize = new System.Drawing.Size(1164, 519);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
