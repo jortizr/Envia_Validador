ec4d42086957dc8ac340f5b331b9e51e83468072
Jefferson Ortiz Rodriguez <jortizr@ut.edu.co>
2025-04-02 00:08:46 -0500

    implementacion de consulta finalizada.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,43 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Validador));
             this.dgvGuias = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
             this.Guia = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Ciudad_Destino = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Guias = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Ciudad_Dest_List = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Ciudad_Dest_Central = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Estado_Guia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnValidarGuias = new System.Windows.Forms.Button();
+            this.progressBarGuias = new System.Windows.Forms.ProgressBar();
+            this.txtPorcentaje = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvGuias)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvGuias
             // 
+            this.dgvGuias.AllowUserToAddRows = false;
             this.dgvGuias.AllowUserToOrderColumns = true;
             this.dgvGuias.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvGuias.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Guia,
-            this.Ciudad_Destino});
-            this.dgvGuias.Location = new System.Drawing.Point(23, 79);
+            this.Ciudad_Destino,
+            this.Ciudad_Dest_Central,
+            this.Estado_Guia});
+            this.dgvGuias.Location = new System.Drawing.Point(15, 30);
             this.dgvGuias.Name = "dgvGuias";
-            this.dgvGuias.Size = new System.Drawing.Size(330, 428);
+            this.dgvGuias.Size = new System.Drawing.Size(663, 403);
             this.dgvGuias.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(20, 59);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Listado de guias a validar";
             // 
             // Guia
             // 
@@ -78,9 +70,33 @@
             this.Ciudad_Destino.HeaderText = "Ciudad Destino";
             this.Ciudad_Destino.Name = "Ciudad_Destino";
             // 
+            // Ciudad_Dest_Central
+            // 
+            this.Ciudad_Dest_Central.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Ciudad_Dest_Central.HeaderText = "Cuiudad Destino (central)";
+            this.Ciudad_Dest_Central.Name = "Ciudad_Dest_Central";
+            // 
+            // Estado_Guia
+            // 
+            this.Estado_Guia.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Estado_Guia.HeaderText = "Estado actual";
+            this.Estado_Guia.Name = "Estado_Guia";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(196, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Listado de guias a validar";
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(359, 79);
+            this.button1.Location = new System.Drawing.Point(684, 30);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(122, 34);
             this.button1.TabIndex = 2;
@@ -90,7 +106,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(359, 119);
+            this.button2.Location = new System.Drawing.Point(684, 70);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(122, 37);
             this.button2.TabIndex = 3;
@@ -98,68 +114,54 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.btnLimpiarLista);
             // 
-            // button3
+            // btnValidarGuias
             // 
-            this.button3.Location = new System.Drawing.Point(359, 162);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(122, 29);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Validar Guias";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnValidarGuias.Location = new System.Drawing.Point(684, 113);
+            this.btnValidarGuias.Name = "btnValidarGuias";
+            this.btnValidarGuias.Size = new System.Drawing.Size(122, 29);
+            this.btnValidarGuias.TabIndex = 4;
+            this.btnValidarGuias.Text = "Validar Guias";
+            this.btnValidarGuias.UseVisualStyleBackColor = true;
+            this.btnValidarGuias.Click += new System.EventHandler(this.btnValidarGuiasAPI);
             // 
-            // dataGridView1
+            // progressBarGuias
             // 
-            this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Guias,
-            this.Ciudad_Dest_List,
-            this.Ciudad_Dest_Central,
-            this.Estado_Guia});
-            this.dataGridView1.Location = new System.Drawing.Point(492, 78);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(660, 429);
-            this.dataGridView1.TabIndex = 5;
+            this.progressBarGuias.Location = new System.Drawing.Point(124, 434);
+            this.progressBarGuias.Name = "progressBarGuias";
+            this.progressBarGuias.Size = new System.Drawing.Size(467, 20);
+            this.progressBarGuias.Step = 1;
+            this.progressBarGuias.TabIndex = 5;
+            this.progressBarGuias.Tag = "";
             // 
-            // Guias
+            // txtPorcentaje
             // 
-            this.Guias.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Guias.HeaderText = "Guia Consultada";
-            this.Guias.Name = "Guias";
-            // 
-            // Ciudad_Dest_List
-            // 
-            this.Ciudad_Dest_List.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Ciudad_Dest_List.HeaderText = "Ciudad Destino(Lista)";
-            this.Ciudad_Dest_List.Name = "Ciudad_Dest_List";
-            // 
-            // Ciudad_Dest_Central
-            // 
-            this.Ciudad_Dest_Central.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Ciudad_Dest_Central.HeaderText = "Ciudad Destino (Central)";
-            this.Ciudad_Dest_Central.Name = "Ciudad_Dest_Central";
-            // 
-            // Estado_Guia
-            // 
-            this.Estado_Guia.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Estado_Guia.HeaderText = "Estado actual";
-            this.Estado_Guia.Name = "Estado_Guia";
+            this.txtPorcentaje.AutoSize = true;
+            this.txtPorcentaje.BackColor = System.Drawing.Color.Transparent;
+            this.txtPorcentaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPorcentaje.ForeColor = System.Drawing.Color.Transparent;
+            this.txtPorcentaje.Location = new System.Drawing.Point(597, 436);
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.Size = new System.Drawing.Size(30, 17);
+            this.txtPorcentaje.TabIndex = 6;
+            this.txtPorcentaje.Text = "0%";
             // 
             // Validador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1164, 519);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.button3);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(832, 459);
+            this.Controls.Add(this.txtPorcentaje);
+            this.Controls.Add(this.progressBarGuias);
+            this.Controls.Add(this.btnValidarGuias);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvGuias);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Validador";
             this.Text = "Validador de datos";
             ((System.ComponentModel.ISupportInitialize)(this.dgvGuias)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -169,16 +171,15 @@
 
         private System.Windows.Forms.DataGridView dgvGuias;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Guia;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Ciudad_Destino;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Guias;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Ciudad_Dest_List;
+        private System.Windows.Forms.Button btnValidarGuias;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Guia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ciudad_Destino;
         private System.Windows.Forms.DataGridViewTextBoxColumn Ciudad_Dest_Central;
         private System.Windows.Forms.DataGridViewTextBoxColumn Estado_Guia;
+        private System.Windows.Forms.ProgressBar progressBarGuias;
+        private System.Windows.Forms.Label txtPorcentaje;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,41 +14,144 @@ namespace Envia_Validador
 {
     public partial class Validador: Form
     {
+        private readonly HttpClient _httpClient = new HttpClient();
+        private readonly string ApiUrlConsulta = "https://hub.envia.co/ServicioRESTConsultaEstados/Service1Consulta.svc/ConsultaGuia/";
+
         public Validador()
         {
             InitializeComponent();
         }
 
-        private void CapturarPortapapeles()
+        private async Task ConsultarEstadoGuia()
         {
-            if (Clipboard.ContainsText())
+            int totalGuias = dgvGuias.Rows.Count;
+            if (totalGuias == 0)
             {
-                string clipboardText = Clipboard.GetText();
-                string[] lineas = clipboardText.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                MessageBox.Show("No hay guias para validar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
 
-                dgvGuias.Rows.Clear();
-                foreach (string linea in lineas)
-                {
-                    string[] columnas = linea.Split('\t');
+            //carga de la barras de progreso
+            progressBarGuias.Value = 0;
+            progressBarGuias.Minimum = 0;
+            progressBarGuias.Maximum = totalGuias;
 
-                    if (columnas.Length == 2)
+            // Actualizar etiqueta al inicio
+            txtPorcentaje.Text = $"0% {totalGuias}";
+            int procesadas = 0;
+
+            foreach (DataGridViewRow fila in dgvGuias.Rows) 
+            {
+                if (fila.Cells[0].Value == null) continue;
+
+                string guia = fila.Cells[0].Value.ToString();
+                string urlConsulta = ApiUrlConsulta + guia;
+
+                try 
+                { 
+                    string jsonRespuesta = await _httpClient.GetStringAsync(urlConsulta);
+                    JObject respuestaGet = JObject.Parse(jsonRespuesta);
+
+                    string ciudadDestinoApi = respuestaGet["ciudad_destino"]?.ToString();
+                    string estadoApi = respuestaGet["estado"]?.ToString();
+
+                    fila.Cells[2].Value = ciudadDestinoApi;
+                    fila.Cells[3].Value = estadoApi;
+
+                    //valida si la ciudad de la lista coincide con la ciudad de la api
+                    if (fila.Cells[1].Value != null && fila.Cells[1].Value.ToString() != ciudadDestinoApi)
                     {
-                        dgvGuias.Rows.Add(columnas[0], columnas[1]);
+                        fila.DefaultCellStyle.BackColor = Color.Red;
                     }
                     else
                     {
-                        MessageBox.Show("los datos pegados no contienen las dos columnas esperadas, vuelve a copiar los datos",
-                            "formato incorrecto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                        dgvGuias.Rows.Clear();
-                        return;
+                        fila.DefaultCellStyle.BackColor = Color.Green;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    fila.DefaultCellStyle.BackColor = Color.Gray;
+                    fila.Cells[3].Value = "Error"; 
+                    MessageBox.Show("Error al consultar la guia " + guia + ": " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                progressBarGuias.Value++;
+                procesadas++;
+
+                //calcular el porcentaje de progreso
+                int porcentaje = (procesadas * 100) / totalGuias;
+                int faltantes = totalGuias - procesadas;
+                txtPorcentaje.Text = $"{porcentaje}% - {faltantes}/{totalGuias}";
+
+                await Task.Delay(50);
+            }
+            txtPorcentaje.Text = "Consulta finalizada ✅";
+            MessageBox.Show("Consulta finalizada exitosamente", "Consulta exitosa", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void CapturarPortapapeles()
+        {
+            try {
+                if (Clipboard.ContainsText())
+                {
+                    string clipboardText = Clipboard.GetText();
+                    string[] lineas = clipboardText.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    dgvGuias.Rows.Clear();
+                    foreach (string linea in lineas)
+                    {
+                        string[] columnas = linea.Split('\t');
+
+                        if (columnas.Length == 2)
+                        {
+                            dgvGuias.Rows.Add(columnas[0], columnas[1].ToUpper());
+                        }
+                        else
+                        {
+                            MessageBox.Show("los datos pegados no contienen las dos columnas esperadas, vuelve a copiar los datos GUIA y CIUDAD-DEPT",
+                                "formato incorrecto", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                            dgvGuias.Rows.Clear();
+                            return;
+                        }
                     }
                 }
 
             }
-            else
+            catch (Exception ex)
             {
+                MessageBox.Show("Error al capturar el portapapeles: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 MessageBox.Show("No hay texto en el portapapeles", "portapapeles vacio", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+ 
+        }
+
+
+        //funcion para organizar las filas que ya tienen un color rojo de primeras y las verdes de ultimas
+        private void OrganizarFilas()
+        {
+            List<DataGridViewRow> filasRojo = new List<DataGridViewRow>();
+            List<DataGridViewRow> filasVerde = new List<DataGridViewRow>();
+            List<DataGridViewRow> filasGris = new List<DataGridViewRow>();
+            foreach (DataGridViewRow fila in dgvGuias.Rows)
+            {
+                if (fila.DefaultCellStyle.BackColor == Color.Red)
+                {
+                    filasRojo.Add(fila);
+                }
+                else if (fila.DefaultCellStyle.BackColor == Color.Green)
+                {
+                    filasVerde.Add(fila);
+                }
+                else
+                {
+                    filasGris.Add(fila);
+                }
+            }
+            dgvGuias.Rows.Clear();
+            dgvGuias.Rows.AddRange(filasRojo.ToArray());
+            dgvGuias.Rows.AddRange(filasGris.ToArray());
+            dgvGuias.Rows.AddRange(filasVerde.ToArray());
         }
 
 
@@ -59,6 +164,14 @@ namespace Envia_Validador
         private void btnLimpiarLista(object sender, EventArgs e)
         {
             dgvGuias.Rows.Clear();
+            txtPorcentaje.Text = "0%";
+        }
+
+        private async void btnValidarGuiasAPI(object sender, EventArgs e)
+        {
+            await ConsultarEstadoGuia();
+            OrganizarFilas();
+
         }
     }
 }
